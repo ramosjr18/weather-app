4d32c4e88b7d94dc51cfc34b1616cf460594d6fd
Daniel Ramos <ramosdarc-18@outlook.com>
2023-05-11 21:34:39 +0200

    almos final design

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [NavBarComponent, SearchBarComponent, Error404PageComponent],
-  exports: [NavBarComponent, SearchBarComponent],
+  declarations: [SearchBarComponent, Error404PageComponent],
+  exports: [SearchBarComponent],
   imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
