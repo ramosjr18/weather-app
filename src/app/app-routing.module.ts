33aa636259939cafbddbf134974eb8bb1c29d03e
Daniel Ramos <ramosdarc-18@outlook.com>
2023-05-13 16:18:32 +0200

    about and contact routes are now working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './main/pages/about-page/about-page.component';
+import { ContactPageComponent } from './main/pages/contact-page/contact-page.component';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
 const routes: Routes = [
   {
     path: 'main',
     loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+  {
+    path: 'about-page',
+    component: AboutPageComponent,
+  },
+  {
+    path: 'contact-page',
+    component: ContactPageComponent,
   },
   {
     path: '404',
@@ -23,4 +33,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
