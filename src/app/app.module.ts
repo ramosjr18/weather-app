8e2286ad3f751626ff4380dda2cd1ab5051172a3
ramosjr18 <106755836+ramosjr18@users.noreply.github.com>
2023-05-31 00:30:43 +0200

    adding weatherInfo Page into Providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainModule } from './main/main.module';
 import { SharedModule } from './shared/shared.module';
+import { WeatherInfoMainPageComponent } from './main/pages/weather-info-main-page/weather-info-main-page.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, MainModule, SharedModule],
-  providers: [],
+  providers: [WeatherInfoMainPageComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
